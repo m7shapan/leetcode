456d85c0699a71b8a377ca4ff15c365ba6a84695
Mohamed Shapan <m7shapan@gmail.com>
2022-10-12 21:42:33 +0200

    Time: 11 ms (12.28%), Space: 3 MB (8.56%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.go
@@ -0,0 +1,30 @@
+func searchInsert(nums []int, target int) int {
+    l := 0 
+    h := len(nums)-1
+    
+    for (h-l) > 1 {
+        mid := (h+l)/2
+        
+        if target > nums[mid] {
+            l = mid+1
+        } else {
+            h = mid
+        }
+    }
+    
+    fmt.Println(l,h)
+    if target <= nums[l] {
+        return l
+    } else if l != h && nums[h] >= target{
+        return h
+    } else {
+        return h+1
+    }
+    
+    return h
+}
+
+// l=0 h=2 m=1
+// 3 > 3 
+// l=2 h=2
+// i1 
