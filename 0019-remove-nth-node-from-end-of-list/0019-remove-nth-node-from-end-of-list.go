e7f96af9c395a11965befa30ebb67fa21ee92f77
Mohamed Shapan <m7shapan@gmail.com>
2022-10-16 16:53:09 +0200

    Time: 3 ms (52.53%), Space: 2.3 MB (33.24%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.go
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.go
@@ -6,23 +6,22 @@
  * }
  */
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-    length := 0
+    length := 0    
     next := head
-    
+    behind := head
     for ; next != nil; next = next.Next {
+        if length > n {
+            behind = behind.Next
+        }
+        
         length++
     }
     
     nth := length-n-1
-    next = head
-    for i:=0;i<nth;i++ {
-        next = next.Next
-    }
-    
     if nth == -1 {
         head = head.Next
-    } else if next.Next != nil {
-        next.Next = next.Next.Next
+    } else if behind.Next != nil {
+        behind.Next = behind.Next.Next
     } else {
         head = nil
     }
